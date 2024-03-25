6317b4fbea8d87935baf494ef103e5173e597d8a
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2024-03-25 10:01:47 +0000

    format + async retry
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- retry/api.py
+++ retry/api.py
@@ -1,6 +1,7 @@
 import logging
 import random
 import time
+import asyncio
 
 from functools import partial
 
@@ -10,8 +11,16 @@ from .compat import decorator
 logging_logger = logging.getLogger(__name__)
 
 
-def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0,
-                     logger=logging_logger):
+def __retry_internal_sync(
+    f,
+    exceptions=Exception,
+    tries=-1,
+    delay=0,
+    max_delay=None,
+    backoff=1,
+    jitter=0,
+    logger=logging_logger,
+):
     """
     Executes a function and retries it if it failed.
 
@@ -37,7 +46,7 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None,
                 raise
 
             if logger is not None:
-                logger.warning('%s, retrying in %s seconds...', e, _delay)
+                logger.warning("%s, retrying in %s seconds...", e, _delay)
 
             time.sleep(_delay)
             _delay *= backoff
@@ -51,7 +60,64 @@ def __retry_internal(f, exceptions=Exception, tries=-1, delay=0, max_delay=None,
                 _delay = min(_delay, max_delay)
 
 
-def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, jitter=0, logger=logging_logger):
+async def __retry_internal_async(
+    f,
+    exceptions=Exception,
+    tries=-1,
+    delay=0,
+    max_delay=None,
+    backoff=1,
+    jitter=0,
+    logger=logging_logger,
+):
+    """
+    Executes a function and retries it if it failed.
+
+    :param f: the function to execute.
+    :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
+    :param tries: the maximum number of attempts. default: -1 (infinite).
+    :param delay: initial delay between attempts. default: 0.
+    :param max_delay: the maximum value of delay. default: None (no limit).
+    :param backoff: multiplier applied to delay between attempts. default: 1 (no backoff).
+    :param jitter: extra seconds added to delay between attempts. default: 0.
+                   fixed if a number, random if a range tuple (min, max)
+    :param logger: logger.warning(fmt, error, delay) will be called on failed attempts.
+                   default: retry.logging_logger. if None, logging is disabled.
+    :returns: the result of the f function.
+    """
+    _tries, _delay = tries, delay
+    while _tries:
+        try:
+            return await f()
+        except exceptions as e:
+            _tries -= 1
+            if not _tries:
+                raise
+
+            if logger is not None:
+                logger.warning("%s, retrying in %s seconds...", e, _delay)
+
+            await asyncio.sleep(_delay)
+            _delay *= backoff
+
+            if isinstance(jitter, tuple):
+                _delay += random.uniform(*jitter)
+            else:
+                _delay += jitter
+
+            if max_delay is not None:
+                _delay = min(_delay, max_delay)
+
+
+def retry(
+    exceptions=Exception,
+    tries=-1,
+    delay=0,
+    max_delay=None,
+    backoff=1,
+    jitter=0,
+    logger=logging_logger,
+):
     """Returns a retry decorator.
 
     :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
@@ -70,15 +136,44 @@ def retry(exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1, ji
     def retry_decorator(f, *fargs, **fkwargs):
         args = fargs if fargs else list()
         kwargs = fkwargs if fkwargs else dict()
-        return __retry_internal(partial(f, *args, **kwargs), exceptions, tries, delay, max_delay, backoff, jitter,
-                                logger)
+        if asyncio.iscoroutinefunction(f):
+            return __retry_internal_async(
+                partial(f, *args, **kwargs),
+                exceptions,
+                tries,
+                delay,
+                max_delay,
+                backoff,
+                jitter,
+                logger,
+            )
+        else:
+            return __retry_internal_sync(
+                partial(f, *args, **kwargs),
+                exceptions,
+                tries,
+                delay,
+                max_delay,
+                backoff,
+                jitter,
+                logger,
+            )
 
     return retry_decorator
 
 
-def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, tries=-1, delay=0, max_delay=None, backoff=1,
-               jitter=0,
-               logger=logging_logger):
+def retry_call(
+    f,
+    fargs=None,
+    fkwargs=None,
+    exceptions=Exception,
+    tries=-1,
+    delay=0,
+    max_delay=None,
+    backoff=1,
+    jitter=0,
+    logger=logging_logger,
+):
     """
     Calls a function and re-executes it if it failed.
 
@@ -98,4 +193,25 @@ def retry_call(f, fargs=None, fkwargs=None, exceptions=Exception, tries=-1, dela
     """
     args = fargs if fargs else list()
     kwargs = fkwargs if fkwargs else dict()
-    return __retry_internal(partial(f, *args, **kwargs), exceptions, tries, delay, max_delay, backoff, jitter, logger)
+    if asyncio.iscoroutinefunction(f):
+        return __retry_internal_async(
+            partial(f, *args, **kwargs),
+            exceptions,
+            tries,
+            delay,
+            max_delay,
+            backoff,
+            jitter,
+            logger,
+        )
+    else:
+        return __retry_internal_sync(
+            partial(f, *args, **kwargs),
+            exceptions,
+            tries,
+            delay,
+            max_delay,
+            backoff,
+            jitter,
+            logger,
+        )
--- retry/compat.py
+++ retry/compat.py
@@ -1,28 +1,40 @@
+
+
+
 import functools
 import logging
+import asyncio
 
+def decorator(caller):
+    """Turns caller into a decorator.
+    Unlike decorator module, function signature is not preserved.
 
-try:
-    from decorator import decorator
-except ImportError:
-    def decorator(caller):
-        """ Turns caller into a decorator.
-        Unlike decorator module, function signature is not preserved.
+    :param caller: caller(f, *args, **kwargs)
+    """
 
-        :param caller: caller(f, *args, **kwargs)
-        """
-        def decor(f):
+    def decor(f):
+        if asyncio.iscoroutinefunction(f):
+
+            @functools.wraps(f)
+            async def wrapper(*args, **kwargs):
+                return await caller(f, *args, **kwargs)
+
+            return wrapper
+        else:
+
             @functools.wraps(f)
             def wrapper(*args, **kwargs):
                 return caller(f, *args, **kwargs)
+
             return wrapper
-        return decor
+
+    return decor
 
 
 try:  # Python 2.7+
     from logging import NullHandler
 except ImportError:
-    class NullHandler(logging.Handler):
 
+    class NullHandler(logging.Handler):
         def emit(self, record):
             pass
